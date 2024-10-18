5a3e799770352ae805bf1a69c10c3863441d4a8c
Christian <115067566+kitearuba@users.noreply.github.com>
2024-10-18 03:25:49 +0200

    Update rot_13.c

--- Level_1/rot_13/rot_13.c
+++ Level_1/rot_13/rot_13.c
@@ -1,21 +1,37 @@
 #include <unistd.h>
 
+void	rot_13(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		// For lowercase letters
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i] + 13 > 'z')
+				str[i] = str[i] - 13;
+			else
+				str[i] = str[i] + 13;
+		}
+		// For uppercase letters
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			if (str[i] + 13 > 'Z')
+				str[i] = str[i] - 13;
+			else
+				str[i] = str[i] + 13;
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	int i = 0;
-
-	if (argc > 1)
-	{
-		while(argv[1][i])
-		{
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = (argv[1][i] - 'a' + 13) % 26 + 'a';
-			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
-			write(1, &argv[1][i], 1);
-			i++;
-		}
-	}
+	if (argc == 2)
+		rot_13(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
