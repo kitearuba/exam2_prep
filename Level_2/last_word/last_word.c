a8ea0e3dae5a5f65e962bf04b95dc1db623028c9
Christian <115067566+kitearuba@users.noreply.github.com>
2024-10-18 05:01:00 +0200

    Update last_word.c

--- Level_2/last_word/last_word.c
+++ Level_2/last_word/last_word.c
@@ -2,23 +2,31 @@
 
 void	last_word(char *str)
 {
-	int j = 0;
-	int i = 0;
+	int	i;
+	int	end;
+	int	start;
 
+	i = 0;
+	// Find the end of the string
 	while (str[i])
-	{
-		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
-			j = i + 1;
 		i++;
-	}
-	while (str[j] >= 33 && str[j] <= 127)
+	// Move backwards to find the last word
+	while (i > 0 && (str[i - 1] == ' ' || str[i - 1] == '\t'))
+		i--;
+	end = i;
+	// Move backwards to find the start of the last word
+	while (i > 0 && str[i - 1] != ' ' && str[i - 1] != '\t')
+		i--;
+	start = i;
+	// Print the last word
+	while (start < end)
 	{
-		write(1, &str[j], 1);
-		j++;
+		write(1, &str[start], 1);
+		start++;
 	}
 }
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		last_word(argv[1]);
