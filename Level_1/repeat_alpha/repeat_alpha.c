e9f4d55aaad13508225489be4e045d97634dd7db
Christian <115067566+kitearuba@users.noreply.github.com>
2024-10-18 03:09:20 +0200

    Update repeat_alpha.c

--- Level_1/repeat_alpha/repeat_alpha.c
+++ Level_1/repeat_alpha/repeat_alpha.c
@@ -1,27 +1,29 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+void	repeat_alpha(char *str)
 {
-    int i;
-    int count;
+	int	i;
+	int	repeat;
 
-    if (ac == 2)
-    {
-        i = 0;
-        while (av[1][i])
-        {
-            if (av[1][i] >= 'a' && av[1][i] <= 'z')
-                count = av[1][i] - 'a' + 1;
-            else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-                count = av[1][i] - 'A' + 1;
-            else
-                count = 1;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			repeat = str[i] - 'a' + 1;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			repeat = str[i] - 'A' + 1;
+		else
+			repeat = 1;
+		while (repeat--)
+			write(1, &str[i], 1);
+		i++;
+	}
+}
 
-            while (count--)
-                write(1, &av[1][i], 1);
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return 0;
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		repeat_alpha(argv[1]);
+	write(1, "\n", 1);
+	return (0);
 }
