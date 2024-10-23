8bab597104d20921ae53f4e11ba1000b9cdf5b52
Christian <115067566+kitearuba@users.noreply.github.com>
2024-10-23 19:23:08 +0200

    Update print_bits.c

--- Level_2/print_bits/print_bits.c
+++ Level_2/print_bits/print_bits.c
@@ -11,3 +11,19 @@ void print_bits(unsigned char octet)
 		write(1, &bit, 1);
 	}
 }
+
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int	i = 8;
+
+	while (i)
+	{
+		i--;
+		if (octet & (1 << i))  // This checks if the i-th bit is 1
+            write(1, "1", 1);  // If it's 1, print '1'
+        else
+            write(1, "0", 1);  // If it's 0, print '0'
+    }
+}
