84b1c622a868a03994c7fd5d9cbbfc8e024dc524
Christian <115067566+kitearuba@users.noreply.github.com>
2024-10-24 18:31:08 +0200

    Update rev_wstr.c

--- Level_4/rev_wstr/rev_wstr.c
+++ Level_4/rev_wstr/rev_wstr.c
@@ -1,33 +1,44 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+void	rev_wstr(char *s)
 {
-	int start;
 	int end;
-	int i = 0;
+
+	end = 0;
+	// Move to the end of the string
+	while (s[end])
+		end++;
+	end--;
+
+	// Start from the end of the string and find words
+	while (end >= 0)
+	{
+		// Skip trailing spaces and tabs
+		while (end >= 0 && (s[end] == ' ' || s[end] == '\t'))
+			end--;
+
+		// Mark the end of the word
+		int word_end = end;
+
+		// Find the start of the word
+		while (end >= 0 && s[end] != ' ' && s[end] != '\t')
+			end--;
+
+		// Print the word
+		write(1, &s[end + 1], word_end - end);
 		
-	if(argc == 2)
-	{   
-		while(argv[1][i] != '\0')
-			i++;
-		while(i >= 0)
-		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
-				i--;
-			end = i;
-			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-				i--;
-			start = i + 1;
-			int  flag;
-			flag = start;
-			while(start <= end)
-			{
-				write (1, &argv[1][start],1);
-				start++;		
-			}
-			if (flag !=0)
-				write(1, " ", 1);
-		}
+		// Print a space after the word if there's more to come
+		if (end > 0)
+			write(1, " ", 1);
 	}
-	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		rev_wstr(argv[1]);
+	}
+	write(1, "\n", 1);  // Always print a newline at the end
+	return (0);
 }
